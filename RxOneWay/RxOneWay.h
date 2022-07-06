6c885d5291e94af94cbd0414b14b52484ac4eb7c
Skyline-23 <bs2740@naver.com>
2022-07-06 14:38:36 +0900

    chore: Add Carthage Files
    Add files to compatible with Carthage

---
+++ RxOneWay/RxOneWay.h
@@ -0,0 +1,18 @@
+//
+//  RxOneWay.h
+//  RxOneWay
+//
+//  Created by 김부성 on 2022/07/06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxOneWay.
+FOUNDATION_EXPORT double RxOneWayVersionNumber;
+
+//! Project version string for RxOneWay.
+FOUNDATION_EXPORT const unsigned char RxOneWayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOneWay/PublicHeader.h>
+
+
